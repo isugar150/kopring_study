cd264ac929fef427705f1efefdd6712e95bf896a
JMKIM <isugar150@gmail.com>
2024-04-01 09:55:34 +0900

    modify project

--- study1/src/main/kotlin/com/example/study1/controller/ApiController.kt
+++ study1/src/main/kotlin/com/example/study1/controller/ApiController.kt
@@ -1,6 +1,5 @@
 package com.example.study1.controller
 
-import com.example.study1.component.constant.HttpMethod
 import com.example.study1.dto.common.RestResult
 import com.example.study1.dto.users.UsersDto
 import com.example.study1.repository.UsersRepository
@@ -9,12 +8,14 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 
@@ -36,8 +37,13 @@ class ApiController {
         return ResponseEntity(result, HttpStatus.OK)
     }
 
-    @PostMapping(value = ["/users"])
-    fun postUsers(param: UsersDto, request: HttpServletRequest): ResponseEntity<RestResult> {
+    @PostMapping(value = ["/users"], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
+    fun postUsers(@io.swagger.v3.oas.annotations.parameters.RequestBody(
+                        content = [
+                            Content(schema = Schema(implementation = UsersDto::class))
+                        ]
+                    )@Valid param: UsersDto
+                  , request: HttpServletRequest): ResponseEntity<RestResult> {
         var result = RestResult()
 
         usersRepository.save(param.toEntity())
--- study1/src/main/kotlin/com/example/study1/dto/users/UsersDto.kt
+++ study1/src/main/kotlin/com/example/study1/dto/users/UsersDto.kt
@@ -1,6 +1,8 @@
 package com.example.study1.dto.users
 
 import com.example.study1.entity.users.UsersEntity
+import com.fasterxml.jackson.annotation.JsonIgnore
+import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 import io.swagger.annotations.ApiOperation
 import io.swagger.v3.oas.annotations.media.Schema
@@ -12,43 +14,43 @@ import java.time.LocalDateTime
 
 @Schema(description = "사용자")
 data class UsersDto(
-    @Schema(description = "사용자 이메일", nullable = false, example = "k12@gmail.com")
+    @JsonIgnore
+    @Schema(description = "사용자 고유번호", nullable = false)
     var id: Long?,
 
-    @ApiModelProperty(value = "로그인 ID")
+    @Schema(description = "로그인 아이디", nullable = false, example = "test")
     var userId: String?,
 
     @NotNull(message = "Password cannot be null")
     @Size(min = 8, message = "Password not be less than two characters")
-    @Schema(description = "사용자 비밀번호", nullable = false, example = "pwd")
-    @ApiModelProperty(value = "비밀번호")
+    @Schema(description = "사용자 비밀번호", nullable = false, example = "test")
     var password: String?,
 
     @NotNull(message = "Name cannot be null")
     @Size(min = 2, message = "Name not be less than two characters")
-    @Schema(description = "사용자 이름", nullable = false, example = "김재한")
+    @Schema(description = "사용자 이름", nullable = false, example = "홍길동")
     var name: String?,
 
     @NotNull(message = "Email cannot be null")
     @Size(min = 2, message = "Email not be less than two characters")
     @Email
-    @Schema(description = "사용자 이메일", nullable = false, example = "k12@gmail.com")
+    @Schema(description = "사용자 이메일", nullable = false, example = "hgd1414@gmail.com")
     var email: String?,
 
     @NotNull(message = "PhoneNumber cannot be null")
     @Schema(description = "사용자 핸드폰 번호", nullable = false, example = "010-1212-3131")
     var phone: String?,
 
-    @ApiModelProperty(value = "생성날짜")
+    @JsonIgnore
     var createdAt: LocalDateTime = LocalDateTime.now(),
 
-    @ApiModelProperty(value = "생성자")
-    var createId: String?,
+    @JsonIgnore
+    var createId: String? = "test",
 
-    @ApiModelProperty(value = "수정날짜")
+    @JsonIgnore
     var updatedAt: LocalDateTime? = LocalDateTime.now(),
 
-    @ApiModelProperty(value = "수정자")
+    @JsonIgnore
     var updateId: String?
 ) {
     fun toEntity(): UsersEntity {
