06133ecd6c6ea62088a737287d275c0c89565eea
JMKIM <isugar150@gmail.com>
2024-04-02 15:35:56 +0900

    modify project

--- study1/src/main/kotlin/com/example/study1/component/swagger/SwaggerConfig.kt
+++ study1/src/main/kotlin/com/example/study1/component/swagger/SwaggerConfig.kt
@@ -1,5 +1,10 @@
 package com.example.study1.component.swagger
 
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.Operation
+import io.swagger.v3.oas.models.PathItem
+import io.swagger.v3.oas.models.responses.ApiResponse
+import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.ApiInfoBuilder
@@ -7,6 +12,8 @@ import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
+import java.util.function.Consumer
+
 
 @Configuration
 class SwaggerConfig {
@@ -21,6 +28,40 @@ class SwaggerConfig {
             .paths(PathSelectors.any())
             .build()
             .useDefaultResponseMessages(false)
+
+    @Bean
+    fun openApiCustomizer(): OpenApiCustomizer {
+        return OpenApiCustomizer { openApi: OpenAPI ->
+            openApi
+                .paths
+                .values
+                .forEach(
+                    Consumer<PathItem> { pathItem: PathItem ->
+                        pathItem.readOperations()
+                            .forEach(
+                                Consumer<Operation> { operation: Operation ->
+                                    operation
+                                        .responses
+                                        .addApiResponse(
+                                            "200",
+                                            ApiResponse().description("조회 성공")
+                                        )
+                                        .addApiResponse(
+                                            "400",
+                                            ApiResponse().description("잘못된 요청")
+                                        )
+                                        .addApiResponse(
+                                            "404",
+                                            ApiResponse().description("유효하지않은 API 주소")
+                                        )
+                                        .addApiResponse(
+                                            "500",
+                                            ApiResponse().description("서버 오류 - 관리자에게 문의하세요.")
+                                        )
+                                })
+                    })
+        }
+    }
 
     private fun swaggerInfo() = ApiInfoBuilder()
         .title("스웨거 테스트")
