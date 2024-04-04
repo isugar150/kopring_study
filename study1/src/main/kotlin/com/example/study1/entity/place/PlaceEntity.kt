0830f2cfb6576afb454c61d84f2d27d89102c571
JMKIM <isugar150@gmail.com>
2024-04-04 14:49:47 +0900

    modify project

--- study1/src/main/kotlin/com/example/study1/entity/place/PlaceEntity.kt
+++ study1/src/main/kotlin/com/example/study1/entity/place/PlaceEntity.kt
@@ -29,6 +29,10 @@ open class PlaceEntity (
     @Column(length = 30, nullable = true)
     open val endDate: LocalDateTime?,
 
+    @OneToMany(targetEntity = PlaceReviewEntity::class)
+    @JoinColumn(name = "placeId")
+    open val reviews: Collection<PlaceReviewEntity>?,
+
     open val createdAt: LocalDateTime = LocalDateTime.now(),
     open val createId: String,
     @Column(nullable = true)
